3d45bcfc48019a8d775a6df7f13426aa3d36103f
caoyanbing <1820120473@qq.com>
2021-10-21 17:02:29 +0800

    模板

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"database/sql"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"text/template"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var url = flag.String("url", "dtd_db_test:5k2VUnDfqLxGnu@tcp(rm-bp1j6307pxpihh49090130.mysql.rds.aliyuncs.com:3306)/tch_cms", "数据库链接")
+var service = flag.String("service", "service", "服务名称")
+var files = []string{
+	"api",
+	"logic",
+	"model",
+}
 
 type TableDec struct {
 	Field string
@@ -44,6 +54,10 @@ func main()  {
 	}
 	if len(tableList) > 0 {
 		for _,v := range tableList {
+
+
+
+
 			err = builder(v, db, ctx)
 			if err != nil {
 				log.Fatalf("生成错误 %v", err)
@@ -57,10 +71,38 @@ func builder(table string, db *sql.DB, ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println(tableDecList)
+	for _, v := range files {
+		temp := bytes.Buffer{}
+		files, err := template.ParseFiles(fmt.Sprintf("./tmpl/%v.tmpl", v))
+		if err != nil {
+			return err
+		}
+		_ = files.ExecuteTemplate(&temp, "table", table)
+		_ = files.ExecuteTemplate(&temp, "service", *service)
+		_ = files.ExecuteTemplate(&temp, "columns", tableDecList)
+		_ = os.MkdirAll(fmt.Sprintf("./builder/%v/", table), 0666)
+		_ = ioutil.WriteFile(fmt.Sprintf("./builder/%v/%v.api", table, table), temp.Bytes(), 0666)
+	}
 	return err
 }
 
+func capitalize(str string) string {
+	var upperStr string
+	vv := []rune(str)
+	for i := 0; i < len(vv); i++ {
+		if i == 0 {
+			if vv[i] >= 97 && vv[i] <= 122 {
+				vv[i] -= 32
+				upperStr += string(vv[i])
+			} else {
+				return str
+			}
+		} else {
+			upperStr += string(vv[i])
+		}
+	}
+	return upperStr
+}
 
 func tableInfo(table string, db *sql.DB, ctx context.Context) ([]TableDec, error) {
 	rows, err := db.QueryContext(ctx, fmt.Sprintf("DESCRIBE %v", table))
